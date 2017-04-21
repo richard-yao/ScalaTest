3f5c1039e70d8f7c6bfc1e06b2bb25c991ecbdab
richard-yao <yaoyxsgf@gmail.com>
2017-04-21 18:37:49 +0800

    adjust some code

--- ScalaTest/src/main/scala/com/tvunetworks/test/CatchException.scala
+++ ScalaTest/src/main/scala/com/tvunetworks/test/CatchException.scala
@@ -3,8 +3,10 @@ package com.tvunetworks.test
 import java.io.FileReader
 import java.io.FileNotFoundException
 import org.apache.log4j.Logger
+import org.apache.log4j.xml.DOMConfigurator
 import java.io.IOException
 import java.io.InputStreamReader
+import scala.io.Source
 
 /**
  * @author RichardYao
@@ -19,13 +21,26 @@ object CatchException {
       if(args.length == 0) {
         throw new Exception("Input parameter empty")
       } else {
-        val file = new FileReader(args(0))
-        var buffer = new Array[Char](1024)
-        val readBuffer = (buffer: Array[Char]) => file.read(buffer)
-        while(readBuffer(buffer) != -1) {
-          for(temp <- buffer;if temp != "") {
-            print(temp)
+        DOMConfigurator.configure(args(0));
+        if(args.length == 3) {
+          val file = new FileReader(args(1))
+          var buffer = new Array[Char](1024)
+          val readBuffer = (buffer: Array[Char]) => file.read(buffer)
+          while(readBuffer(buffer) != -1) {
+            for(temp <- buffer; if temp.hashCode() != 0) {
+              print(temp)
+            }
           }
+          logger.info("-------Print file content------")
+          val printChar = (char: Char) => print(char)
+          Source.fromFile(args(1)).foreach(printChar)
+          logger.info("-------Print file content------")
+          val lines = Source.fromFile(args(2))("UTF-8")
+          for (line <-lines) {
+            print(line)
+          }
+        } else {
+          throw new Exception("Input parameters' number is not match!")
         }
       }
     } catch {
@@ -39,7 +54,7 @@ object CatchException {
         logger.error(ex)
       }
     } finally {
-      println("Program over")
+      println("程序结束")
     }
   }
 }
